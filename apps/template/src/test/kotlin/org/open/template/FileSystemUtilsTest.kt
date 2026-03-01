fd968d0a913fbd261123d98b982a29ab60c80b68
Mike Resnik <mtresnik@users.noreply.github.com>
2026-03-01 11:15:06 -0500

    Ignores tests.

--- apps/template/src/test/kotlin/org/open/template/FileSystemUtilsTest.kt
+++ apps/template/src/test/kotlin/org/open/template/FileSystemUtilsTest.kt
@@ -1,6 +1,7 @@
 package org.open.template
 
 import org.open.file.template.utils.FileSystemUtils
+import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertNotNull
 
@@ -13,15 +14,19 @@ class FileSystemUtilsTest {
         assertNotNull(file, "File was null!")
     }
 
-    @Test
+//    @Test
+    @Ignore
     fun `user home replacement documents projects`() {
+        // replace with a common directory on your machine
         val path = "~/Documents/Projects"
         val file = FileSystemUtils.inferPath(path)
         assertNotNull(file, "File was null!")
     }
 
-    @Test
+//    @Test
+    @Ignore
     fun `user home replacement documents projects open template`() {
+        // replace with a common directory on your machine
         val path = "~/Documents/Projects/open-file"
         val file = FileSystemUtils.inferPath(path)
         assertNotNull(file, "File was null!")
